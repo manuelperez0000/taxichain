bd7f9866cf8fc9834ec2912ef502836c31074870
Oscar Guerra <oscarluisgm2@gmail.com>
2024-07-12 18:30:45 -0400

    another commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
-
-
-const irons=['0x460A252B4fEEFA821d3351731220627D7B7d1F3d', '0x460A252B4fEEFA821d3351731220627D7B7d1F3d', '0x460A252B4fEEFA821d3351731220627D7B7d1F3d', '0x460A252B4fEEFA821d3351731220627D7B7d1F3d']
+const irons = [
+  "0x460A252B4fEEFA821d3351731220627D7B7d1F3d",
+  "0x460A252B4fEEFA821d3351731220627D7B7d1F3d",
+  "0x460A252B4fEEFA821d3351731220627D7B7d1F3d",
+  "0x460A252B4fEEFA821d3351731220627D7B7d1F3d",
+];
 
 function App() {
-  
   return (
     <>
-    <div className="mine-container">
-      <main>
-        <h1>Taxichain Iron Miner</h1>
-        <div className="second">
-        <h2>Halving:000</h2>
-        <h3>Last decoded block: 0x0x</h3>
-        </div> 
-      </main>
-      <h3>Irons Mined</h3>
-      <div className="container">
-        {irons.map((iron, i)=>{
-          return <div className="cards" key={i}>{iron}</div>})}
-        
-               
+      <div className="mine-container">
+        <div className="clontainer">
+          <main>
+            <h1>Taxichain Iron Miner</h1>
+            <div className="second">
+              <h2>Halving:000</h2>
+              <h3>Last decoded block: 0x0x</h3>
+            </div>
+          </main>
+          <h3>Irons Mined</h3>
+          <div>
+            {irons.map((iron, i) => {
+              return (
+                <div className="cards" key={i}>
+                  {iron}
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
